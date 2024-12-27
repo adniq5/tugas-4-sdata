5bbbab6de6c85dd619621522e67cbda9c56eb1a2
adniq5 <adniq55@gmail.com>
2024-12-27 17:55:06 +0800

    U:Mengubah Tampilan Menu

--- Mydata.cpp
+++ Mydata.cpp
@@ -2,56 +2,56 @@
 #include <string>
 #include <conio.h>
 using namespace std;
-struct mahasiswa {
-    string nim;
-    string nama;
-    string alamat;
-    float ipk;
-};
-mahasiswa sikc[30];
-int pos=-1;
-void tampilkanData(int p){
-    cout << sikc[p].nim<<" , "<< sikc[p].nama<<" , "<< sikc[p].alamat<<" , "<<
-sikc[p].ipk<<endl;
+
+void dMenu(){
+system("cls");
+cout << "\n=== Menu Aplikasi Data Mahasiswa ===\n";
+cout << "1. Masukkan Data Mahasiswa\n";
+cout << "2. Tampilkan Data Mahasiswa\n";
+cout << "3. Update Data Mahasiswa\n";
+cout << "4. Hapus Data Mahasiswa\n";
+cout << "5. Exit\n";
+cout << "Masukkan pilihan: ";  
 }
-void masukanData(){
-    pos++;
-    system("cls");
-    fflush(stdin);
-    cout << "masukan nim: ";
-    getline (cin, sikc[pos].nim);
-    cout << "masukan nama: ";
-    getline (cin, sikc[pos].nama);
-    cout << "masukan alamat: ";
-    getline (cin, sikc[pos].alamat);
-    cout << "masukan ipk: ";
-    cin >> sikc[pos].ipk;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-void perbaikanData(int p)
-{
-    system("cls");
-    fflush(stdin);
-    cout << "masukan nama: ";
-    getline (cin, sikc[p].nama);
-    cout << "masukan alamat: ";
-    getline (cin, sikc[p].alamat);
-    cout << "masukan ipk: ";
-    cin >> sikc[p].ipk;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+void mPertama(string pesan){
+system("cls");
+cout<<"hallo saya menu "<<pesan;
+getch();
 }
 int main() {
- // ujicoba memasukan data di array index pertama
-masukanData();
- // ujicoba memasukan data di array index kedua
- masukanData();
- //ujicoba tampilkan data terakhir input
- tampilkanData(pos);
- // ujicoba tampilkan data pada array pertama
- tampilkanData(0);
-// ujicoba merubah data pada array pertama
-perbaikanData(0);
- // ujicoba merubah data pada array kedua
-tampilkanData(0);
- return 0;
+char pl;
+do
+{
+    dMenu();
+    pl=getch();
+  switch (pl)
+  {
+   case '1':
+    /* code */
+    mPertama("pertama");
+    break;
+   case '2':
+    mPertama("ke- dua");
+    /* code */ 
+    break;  
+   case '3':
+    mPertama("ke- tiga");
+    /* code */
+    break;  
+   case '4':
+    mPertama("ke- empat");
+    /* code */
+    break;  
+  case '5':
+    /* code */
+    break;
+  
+  default:
+    system("cls");
+    cout<<"Pilihan Tidak Tersedia";
+    getch();
+    break;
+  }
+} while (pl!='5');
+  return 0;
 }
